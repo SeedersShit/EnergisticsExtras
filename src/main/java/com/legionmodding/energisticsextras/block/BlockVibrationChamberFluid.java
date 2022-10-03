bc91cc58cdb43a34c115760b48d39bb84a6419dd
LegionModding <legion@legionmodding.com>
2022-10-03 05:54:44 +0100

    Model work

--- src/main/java/com/legionmodding/energisticsextras/block/BlockVibrationChamberFluid.java
+++ src/main/java/com/legionmodding/energisticsextras/block/BlockVibrationChamberFluid.java
@@ -1,12 +1,51 @@
 package com.legionmodding.energisticsextras.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 
 public class BlockVibrationChamberFluid extends Block
 {
+
+    private static final DirectionProperty FACING = HorizontalBlock.FACING;
+    private static final BooleanProperty STATE = BooleanProperty.create("state");
     public BlockVibrationChamberFluid()
     {
         super(Block.Properties.of(Material.HEAVY_METAL));
+        this.registerDefaultState(this.getStateDefinition().any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder)
+    {
+        super.createBlockStateDefinition(builder);
+        builder.add(FACING);
+        builder.add(STATE);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context)
+    {
+        return this.defaultBlockState().setValue(FACING, context.getNearestLookingDirection().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rot)
+    {
+        return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirrorIn)
+    {
+        return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
     }
 }
