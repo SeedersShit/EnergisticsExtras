f40275b4b85d7013019ce335cb3811d96d01a10d
LegionModding <legion@legionmodding.com>
2022-10-04 02:50:01 +0100

    Fix for grower model

--- src/main/java/com/legionmodding/energisticsextras/block/BlockGrower.java
+++ src/main/java/com/legionmodding/energisticsextras/block/BlockGrower.java
@@ -1,12 +1,27 @@
 package com.legionmodding.energisticsextras.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.Direction;
+
+import javax.annotation.Nullable;
 
 public class BlockGrower extends Block
 {
+    private static final BooleanProperty ACTIVE = BooleanProperty.create("active");
     public BlockGrower()
     {
         super(Block.Properties.of(Material.HEAVY_METAL));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder)
+    {
+        super.createBlockStateDefinition(builder);
+        builder.add(ACTIVE);
     }
 }
