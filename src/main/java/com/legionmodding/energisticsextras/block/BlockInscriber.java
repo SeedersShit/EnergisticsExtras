21b3760c6304757e592b90693187f09a4ea957c4
LegionModding <legion@legionmodding.com>
2022-10-04 03:50:05 +0100

    Fix for inscriber model

--- src/main/java/com/legionmodding/energisticsextras/block/BlockInscriber.java
+++ src/main/java/com/legionmodding/energisticsextras/block/BlockInscriber.java
@@ -1,12 +1,23 @@
 package com.legionmodding.energisticsextras.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
 
 public class BlockInscriber extends Block
 {
+    private static final BooleanProperty ACTIVE = BooleanProperty.create("active");
     public BlockInscriber()
     {
         super(Block.Properties.of(Material.HEAVY_METAL));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder)
+    {
+        builder.add(ACTIVE);
+        super.createBlockStateDefinition(builder);
     }
 }
