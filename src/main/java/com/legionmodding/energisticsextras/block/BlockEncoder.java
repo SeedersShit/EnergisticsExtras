bd0a7e61b678647b298563285e2bba7532bcd9c6
LegionModding <legion@legionmodding.com>
2022-10-04 04:00:26 +0100

    Fix for encoder model

--- src/main/java/com/legionmodding/energisticsextras/block/BlockEncoder.java
+++ src/main/java/com/legionmodding/energisticsextras/block/BlockEncoder.java
@@ -1,12 +1,51 @@
 package com.legionmodding.energisticsextras.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 
 public class BlockEncoder extends Block
 {
+    private static final BooleanProperty ACTIVE = BooleanProperty.create("active");
+    private static final DirectionProperty FACING = HorizontalBlock.FACING;
+
     public BlockEncoder()
     {
         super(Block.Properties.of(Material.HEAVY_METAL));
+        this.registerDefaultState(this.getStateDefinition().any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder)
+    {
+        super.createBlockStateDefinition(builder);
+        builder.add(ACTIVE);
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context)
+    {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rot)
+    {
+        return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirrorIn)
+    {
+        return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
     }
 }
